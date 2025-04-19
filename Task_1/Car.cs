61958b9d05db70c09d0f07322b90891ed5c1f1e7
win 10-11 <win 10-11@DESKTOP-QPJAVDM>
2025-04-19 23:58:59 +0200

    Add project files.

---
+++ Task_1/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Car
+    {
+        public string model;
+        public string brand;
+        public string year;
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Task_1;
+
+namespace CIS
+{
+    class Task_1
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car();
+            car1.brand = "Fiat";
+            car1.model = "128";
+            car1.year = "1965";
+
+            Console.WriteLine(car1.brand);
+            Console.WriteLine(car1.model);
+            Console.WriteLine(car1.year);
+
+            Car car2 = new Car();
+            car2.brand = "Mercedes";
+            car2.model = "G-Class";
+            car2.year = "2023";
+
+            Console.WriteLine(car2.brand);
+            Console.WriteLine(car2.model);
+            Console.WriteLine(car2.year);
+        }
+    }
+}
